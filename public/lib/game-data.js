816aaafef9dea5824c28b408e73108e9b661bd57
Bobby Gerace <rfgerace@gmail.com>
2026-02-12 21:56:11 -0500

    Put files back to root

--- public/lib/game-data.js
+++ public/lib/game-data.js
@@ -33,8 +33,8 @@ export class GameData {
   static async preloadDataBank() {
     if (cachedGameList && cachedWordList) return;
 
-    const wordsPromise = fetch("../dictionary.csv").then((res) => res.text());
-    const gamesPromise = fetch("../game_stats.csv").then((res) => res.text());
+    const wordsPromise = fetch("/dictionary.csv").then((res) => res.text());
+    const gamesPromise = fetch("/game_stats.csv").then((res) => res.text());
 
     // Simulate long load time
     // await new Promise((resolve) => setTimeout(resolve, 10000));
